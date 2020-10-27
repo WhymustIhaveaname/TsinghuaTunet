034e05bd5a734a534205123af6a11a242d18849b
Lenovo <you@example.com>
2020-10-28 01:26:32 +0800

    rewrite whole codes.

---
+++ README.md
@@ -0,0 +1,30 @@
+清华校园网自动连接脚本.
+Tsinghua Tunet auto-connect script.
+
+### Usage
+
+* `./connect.py --gen-config`: Generate a configure file including your username and password's hash.
+* `./connect.py --connect`   : Connect directly.
+* `./connect.py --test-first`: First test network by getting a PKU webpage, connect if failed.
+
+First, use `./connect.py --gen-config` to generate a configure file. Then try `./connect.py --connect` or `./connect.py --test-first`.
+
+### Feature
+
+* Short so fewer bugs.
+* Able to test network and reconnect if the test fails.
+* Save password's hash rather than plaintext.
+
+### 使用方法
+
+* `./connect.py --gen-config`: 生成配置文件.
+* `./connect.py --connect`   : 直接连接.
+* `./connect.py --test-first`: 先测试网络, 不通再连接.
+
+首先用 `./connect.py --gen-config` 按提示输入用户名密码生成配置文件, 之后用 `./connect.py --connect` 或 `./connect.py --test-first` 连接.
+
+### 亮点
+
+* 更短, 所以更少的八阿哥.
+* 可以先测试网络是否通畅失败才重连.
+* 保存密码的哈希而不是明文.
---
+++ connect.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import traceback,time,math
+
+LOGFILE="connect.log"
+LOGLEVEL=("DEBUG","INFO","WARNING","ERROR","FATAL")
+
+def log(msg,l=1,end="\n",logfile=LOGFILE):
+    st=traceback.extract_stack()[-2]
+    lstr=LOGLEVEL[l]
+    now_str="%s %03d"%(time.strftime("%y/%m/%d %H:%M:%S",time.localtime()),math.modf(time.time())[0]*1000)
+    if l<3:
+        tempstr="%s [%s,%s:%d] %s%s"%(now_str,lstr,st.name,st.lineno,str(msg),end)
+    else:
+        tempstr="%s [%s,%s:%d] %s:\n%s%s"%(now_str,lstr,st.name,st.lineno,str(msg),traceback.format_exc(limit=5),end)
+    print(tempstr,end="")
+    if l>=1:
+        with open(logfile,"a") as f:
+            f.write(tempstr)
+
+import requests,sys,json
+
+headers={"Accept":"*/*","Host":"net.tsinghua.edu.cn"}
+
+def net_login(username,password_hash):
+    """
+        request: http://net.tsinghua.edu.cn/do_login.php
+        method: POST
+        post data:
+            action: login
+            username: username
+            password: {MD5_HEX}32_bit_hex
+            ac_id: 1
+    """
+    url='https://net.tsinghua.edu.cn/do_login.php'
+    data={'action':'login','username':username,'password':password_hash,'ac_id':'1'}
+    log("posting: %s"%(url))
+    post=requests.post(url,headers=headers,data=data)
+    content=post.content.decode(post.encoding)
+    log('%d: "%s"'%(post.status_code,content))
+    return content
+
+def test_network(test_url):
+    try:
+        log("getting: %s"%(test_url))
+        get=requests.get(test_url,headers=headers,timeout=10)
+        if get.status_code==200:
+            return 0
+        else:
+            return str(get.status_code)
+    except Exception as e:
+        return str(e)
+
+def test_and_reconnent(username,password_hash):
+    """test online or not, if not, login"""
+    import random
+    url_pool=["https://its.pku.edu.cn/","http://hpc.pku.edu.cn/"]
+    test_re=test_network(random.choice(url_pool))
+    if test_re==0:
+        log("online already")
+    else:
+        log("not online(%s), reconnecting..."%(test_re))
+        net_login(username,password_hash)
+
+def gen_config():
+    import getpass,hashlib
+    username=input("username: ")
+    password=getpass.getpass("password: ")
+    password_hash='{MD5_HEX}'+hashlib.md5(password.encode('latin1')).hexdigest()
+    with open("config.json","w") as f:
+        config=json.dump({"username":username,"password_hash":password_hash},f)
+    log("dumped username and password's hash to config.json")
+
+if __name__ == '__main__':
+    if len(sys.argv)>=2 and sys.argv[1]=="--test-first":
+        with open("config.json","r") as f:
+            config=json.load(f)
+        username=config["username"]
+        password_hash=config["password_hash"]
+        test_and_reconnent(username,password_hash)
+    elif len(sys.argv)>=2 and sys.argv[1]=="--connect":
+        with open("config.json","r") as f:
+            config=json.load(f)
+        username=config["username"]
+        password_hash=config["password_hash"]
+        net_login(username,password_hash)
+    elif len(sys.argv)>=2 and sys.argv[1]=="--gen-config":
+        gen_config()
+    else:
+        log("Tsinghua Tunet auto-connect script\n--test-first\n--connect\n--gen-config")
